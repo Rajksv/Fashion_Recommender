c14ddf909914f2a75c54fcf6922646b600f4aa8f
Rajkishore-BITSPilani <H20220802@pilani.bits-pilani.ac.in>
2023-11-21 02:29:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if uploaded_file is not None:
         col1, col2, col3, col4, col5 = st.columns(5)
 
         for i, col in enumerate([col1, col2, col3, col4, col5]):
-            st.header(f"Image {i+1}")
+            
             recommended_image_path = img_files_list[indices[0][i]]
 
             recommended_image_path = os.path.normpath(recommended_image_path)
