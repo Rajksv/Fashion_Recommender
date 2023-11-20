367c5fda70bc1a01dc5d6d1f6741c2a8c3f024dd
Rajkishore-BITSPilani <H20220802@pilani.bits-pilani.ac.in>
2023-11-21 00:40:34 +0530

    .

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input as preprocess_resnet50
-from tensorflow.keras.applications.resnet import ResNet152, preprocess_input as preprocess_resnet152
 from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input as preprocess_dense121
 from tensorflow.keras.models import Sequential
 import numpy as np
@@ -10,22 +9,16 @@ import os
 from tqdm import tqdm
 import pickle
 
-# Initialize models
+# Load ResNet50 and DenseNet121 models
 resnet50_model = ResNet50(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
-resnet152_model = ResNet152(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 dense121_model = DenseNet121(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 
-# Set models as non-trainable
 resnet50_model.trainable = False
-resnet152_model.trainable = False
 dense121_model.trainable = False
 
-# Create Sequential models with GlobalMaxPooling2D layer
 resnet50_extractor = Sequential([resnet50_model, GlobalMaxPooling2D()])
-resnet152_extractor = Sequential([resnet152_model, GlobalMaxPooling2D()])
 dense121_extractor = Sequential([dense121_model, GlobalMaxPooling2D()])
 
-# Function to extract features
 def extract_features(img_path, model, preprocess_func):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
@@ -33,33 +26,21 @@ def extract_features(img_path, model, preprocess_func):
     preprocessed_img = preprocess_func(expand_img)
     result_to_model = model.predict(preprocessed_img)
     flatten_result = result_to_model.flatten()
-    # Normalizing
     result_normalized = flatten_result / norm(flatten_result)
     return result_normalized
 
-# List of image files
-count = 0
 img_files = []
-
 for fashion_images in os.listdir('fashion_small/images'):
-    count+=1
-    if count > 10000:
-        break
     images_path = os.path.join('fashion_small/images', fashion_images)
     img_files.append(images_path)
 
-# Extracting and combining image features
-combined_features = []
+image_features = []
 
 for files in tqdm(img_files):
     features_resnet50 = extract_features(files, resnet50_extractor, preprocess_resnet50)
-    features_resnet152 = extract_features(files, resnet152_extractor, preprocess_resnet152)
     features_dense121 = extract_features(files, dense121_extractor, preprocess_dense121)
-    
-    # Combine features
-    combined = np.concatenate([features_resnet50, features_resnet152, features_dense121])
-    combined_features.append(combined)
+    combined = np.concatenate([features_resnet50, features_dense121])
+    image_features.append(combined)
 
-# Saving combined features and file paths
-pickle.dump(combined_features, open("image_features_embedding.pkl", "wb"))
+pickle.dump(image_features, open("image_features_embedding.pkl", "wb"))
 pickle.dump(img_files, open("img_files.pkl", "wb"))
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PIL import Image
 import os
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input as preprocess_resnet50
-from tensorflow.keras.applications.resnet import ResNet152, preprocess_input as preprocess_resnet152
 from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input as preprocess_dense121
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.models import Sequential
@@ -18,20 +17,14 @@ img_files_list = pickle.load(open("img_files.pkl", "rb"))
 
 # Initialize models
 resnet50_model = ResNet50(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
-resnet152_model = ResNet152(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 dense121_model = DenseNet121(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 
-# Set models as non-trainable
 resnet50_model.trainable = False
-resnet152_model.trainable = False
 dense121_model.trainable = False
 
-# Create Sequential models with GlobalMaxPooling2D layer
 resnet50_extractor = Sequential([resnet50_model, GlobalMaxPooling2D()])
-resnet152_extractor = Sequential([resnet152_model, GlobalMaxPooling2D()])
 dense121_extractor = Sequential([dense121_model, GlobalMaxPooling2D()])
 
-# Function to extract features
 def extract_features(img_path, model, preprocess_func):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
@@ -39,33 +32,27 @@ def extract_features(img_path, model, preprocess_func):
     preprocessed_img = preprocess_func(expand_img)
     result_to_model = model.predict(preprocessed_img)
     flatten_result = result_to_model.flatten()
-    # Normalizing
     result_normalized = flatten_result / norm(flatten_result)
     return result_normalized
 
 st.title('Clothing recommender system')
 
-# Load models for feature extraction
 model_dict = {
     "ResNet50": (resnet50_extractor, preprocess_resnet50),
-    "ResNet152": (resnet152_extractor, preprocess_resnet152),
     "DenseNet121": (dense121_extractor, preprocess_dense121)
 }
 
 uploaded_file = st.file_uploader("Choose your image")
 if uploaded_file is not None:
     try:
-        # Save the uploaded image
         img_path = os.path.join("uploader", uploaded_file.name)
         with open(img_path, 'wb') as f:
             f.write(uploaded_file.getbuffer())
 
-        # Display uploaded image
         show_image = Image.open(uploaded_file)
         resized_image = show_image.resize((400, 400))
         st.image(resized_image)
 
-        # Extract features of uploaded image using all models and concatenate
         combined_features_uploaded = []
         for model_name, (extractor, preprocess_func) in model_dict.items():
             features = extract_features(img_path, extractor, preprocess_func)
@@ -73,12 +60,10 @@ if uploaded_file is not None:
 
         combined_features_uploaded = np.concatenate(combined_features_uploaded)
 
-        # Find similar images using Nearest Neighbors
         neighbors = NearestNeighbors(n_neighbors=5, algorithm='brute', metric='euclidean')
         neighbors.fit(combined_features)
         distances, indices = neighbors.kneighbors([combined_features_uploaded])
 
-        # Display recommended images
         col1, col2, col3, col4, col5 = st.columns(5)
 
         for i, col in enumerate([col1, col2, col3, col4, col5]):
