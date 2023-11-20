d8e44ba406cc40ef91cac757ebd101be8f71819a
Rajkishore-BITSPilani <H20220802@pilani.bits-pilani.ac.in>
2023-11-21 00:09:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,92 @@
 import streamlit as st
-import tensorflow
-import pandas as pd
-from PIL import Image
-import pickle
 import numpy as np
+import pickle
+from PIL import Image
+import os
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import ResNet50,ResNet152,VGG16,Dense121, preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input as preprocess_resnet50
+from tensorflow.keras.applications.resnet import ResNet152, preprocess_input as preprocess_resnet152
+from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input as preprocess_dense121
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.models import Sequential
 from numpy.linalg import norm
 from sklearn.neighbors import NearestNeighbors
-import os
 
-features_list = pickle.load(open("image_features_embedding.pkl", "rb"))
+# Load combined features and image file paths
+combined_features = pickle.load(open("combined_features.pkl", "rb"))
 img_files_list = pickle.load(open("img_files.pkl", "rb"))
 
-model = ResNet50(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
-model.trainable = False
-model = Sequential([model, GlobalMaxPooling2D()])
+# Initialize models
+resnet50_model = ResNet50(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+resnet152_model = ResNet152(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+dense121_model = DenseNet121(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
 
-st.title('Clothing recommender system')
+# Set models as non-trainable
+resnet50_model.trainable = False
+resnet152_model.trainable = False
+dense121_model.trainable = False
 
+# Create Sequential models with GlobalMaxPooling2D layer
+resnet50_extractor = Sequential([resnet50_model, GlobalMaxPooling2D()])
+resnet152_extractor = Sequential([resnet152_model, GlobalMaxPooling2D()])
+dense121_extractor = Sequential([dense121_model, GlobalMaxPooling2D()])
 
-def save_file(uploaded_file):
-    st.header("Here")
-    try:
-        with open(os.path.join("uploader", uploaded_file.name), 'wb') as f:
-            f.write(uploaded_file.getbuffer())
-        return 1
-    except:
-        return 0
-
-
-def extract_img_features(img_path, model):
+# Function to extract features
+def extract_features(img_path, model, preprocess_func):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     expand_img = np.expand_dims(img_array, axis=0)
-    preprocessed_img = preprocess_input(expand_img)
-    result_to_resnet = model.predict(preprocessed_img)
-    flatten_result = result_to_resnet.flatten()
-    # normalizing
-    result_normlized = flatten_result / norm(flatten_result)
+    preprocessed_img = preprocess_func(expand_img)
+    result_to_model = model.predict(preprocessed_img)
+    flatten_result = result_to_model.flatten()
+    # Normalizing
+    result_normalized = flatten_result / norm(flatten_result)
+    return result_normalized
 
-    return result_normlized
+st.title('Clothing recommender system')
 
-
-def recommendd(features, features_list):
-    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
-    neighbors.fit(features_list)
-
-    distence, indices = neighbors.kneighbors([features])
-
-    return indices
+# Load models for feature extraction
+model_dict = {
+    "ResNet50": (resnet50_extractor, preprocess_resnet50),
+    "ResNet152": (resnet152_extractor, preprocess_resnet152),
+    "DenseNet121": (dense121_extractor, preprocess_dense121)
+}
 
 uploaded_file = st.file_uploader("Choose your image")
 if uploaded_file is not None:
-    if save_file(uploaded_file):
-        # display image
-        show_images = Image.open(uploaded_file)
-        size = (400, 400)
-        resized_im = show_images.resize(size)
-        st.image(resized_im)
-        # extract features of uploaded image
-        features = extract_img_features(os.path.join("uploader", uploaded_file.name), model)
-        #st.text(features)
-        img_indicess = recommendd(features, features_list)
-        col1,col2,col3,col4,col5 = st.columns(5)
+    try:
+        # Save the uploaded image
+        img_path = os.path.join("uploader", uploaded_file.name)
+        with open(img_path, 'wb') as f:
+            f.write(uploaded_file.getbuffer())
 
-        with col1:
-            st.header("I")
-            st.image(img_files_list[img_indicess[0][0]])
+        # Display uploaded image
+        show_image = Image.open(uploaded_file)
+        resized_image = show_image.resize((400, 400))
+        st.image(resized_image)
 
-        with col2:
-            st.header("II")
-            st.image(img_files_list[img_indicess[0][1]])
+        # Extract features of uploaded image using all models and concatenate
+        combined_features_uploaded = []
+        for model_name, (extractor, preprocess_func) in model_dict.items():
+            features = extract_features(img_path, extractor, preprocess_func)
+            combined_features_uploaded.append(features)
 
-        with col3:
-            st.header("III")
-            st.image(img_files_list[img_indicess[0][2]])
+        combined_features_uploaded = np.concatenate(combined_features_uploaded)
 
-        with col4:
-            st.header("IV")
-            st.image(img_files_list[img_indicess[0][3]])
+        # Find similar images using Nearest Neighbors
+        neighbors = NearestNeighbors(n_neighbors=5, algorithm='brute', metric='euclidean')
+        neighbors.fit(combined_features)
+        distances, indices = neighbors.kneighbors([combined_features_uploaded])
 
-        with col5:
-            st.header("V")
-            st.image(img_files_list[img_indicess[0][4]])
-    else:
-        st.header("Some error occur")
+        # Display recommended images
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        for i, col in enumerate([col1, col2, col3, col4, col5]):
+            st.header(f"Image {i+1}")
+            recommended_image_path = img_files_list[indices[0][i]]
+            recommended_image = Image.open(recommended_image_path)
+            resized_recommended_image = recommended_image.resize((200, 200))
+            col.image(resized_recommended_image)
+
+    except Exception as e:
+        st.error(f"Error: {e}")
