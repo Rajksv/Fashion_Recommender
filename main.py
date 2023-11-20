44c46531d4eb9f1785cc78795f6801a52ff47b42
Rajkishore-BITSPilani <H20220802@pilani.bits-pilani.ac.in>
2023-11-20 17:27:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,9 @@ st.title('Clothing recommender system')
 def save_file(uploaded_file):
     st.header("Here")
     try:
-        
         with open(os.path.join("uploader", uploaded_file.name), 'wb') as f:
-            f.write(uploaded_file.getbuffer())
-            return 1
+        f.write(uploaded_file.getbuffer())
+        return 1
     except:
         return 0
 
